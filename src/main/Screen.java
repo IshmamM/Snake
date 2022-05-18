144adc37de668b3afe38237b8f7934d80f58361e
Ishmam M <ishmamm2@nycstudents.net>
2022-05-18 10:52:01 -0400

    ADDED KEYEVENTS AND UPDATE DRAWING PER FRAME

--- src/main/Screen.java
+++ src/main/Screen.java
@@ -1,8 +1,7 @@
 package main;
 
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 
 public class Screen extends JPanel implements Runnable{
 
@@ -15,12 +14,23 @@ public class Screen extends JPanel implements Runnable{
     final int screenWidth = maxScreenCol*tileSize;
     final int screenHeight = maxScreenRow*tileSize;
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //Default Starting Position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    //FPS
+    int FPS = 60;
 
     public Screen(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void initiateThread(){
@@ -30,8 +40,43 @@ public class Screen extends JPanel implements Runnable{
 
     @Override
     public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
         while(gameThread != null){
-            System.out.println("Running");
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime)/drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
         }
+    }
+    public void update(){
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed){
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed){
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed){
+            playerX += playerSpeed;
+        }
+
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
+        g2.dispose();
     }
 }
--- src/main/SnakeRunner.java
+++ src/main/SnakeRunner.java
@@ -18,6 +18,6 @@ public class SnakeRunner {
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
-        Screen.initiateThread();
+        screen.initiateThread();
     }
 }
